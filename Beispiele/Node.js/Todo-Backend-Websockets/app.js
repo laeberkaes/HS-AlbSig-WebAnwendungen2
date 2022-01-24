658395fa9fa8fa4281518790fb8913b96442acea
hnshbr <huberhan@hs-albsig.de>
2022-01-24 09:21:06 +0100

    ...

--- Beispiele/Node.js/Todo-Backend-Websockets/app.js
+++ Beispiele/Node.js/Todo-Backend-Websockets/app.js
@@ -22,8 +22,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
 app.get("/v1/todos", async (req, res) => {
     const todoList = todoDb.prepare("SELECT * FROM todos").all();
 
